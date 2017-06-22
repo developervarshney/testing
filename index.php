2c90e9c152d74f24e5f7194b017a2673a722e7e4
nitin <nitin.sethi@amvrin.com>
2017-06-22 12:23:04 +0530

    first commit index file

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+
+require_once('DNCManager.class.php');
+
+$_dnc_url = null; // Default
+if (isset($dev) && $dev != false) {
+	$_dnc_url = 'http://dnc-system/dnc.api';
+}
+$_dnc = DncManager::shared('3E142E0EF6A7524090C9074A17F3A7A55AFD9FB8F60111E6143C9CC06A814005', $_dnc_url);
+
+?>
+
+
